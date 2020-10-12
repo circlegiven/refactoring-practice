068480e1339c2f685246d2e3362a2402271aad56
circlegiven <circlegiven@gmail.com>
2020-10-12 20:55:09 +0900

    연산 처리 함수로 이동

--- examples/playground.js
+++ examples/playground.js
@@ -10,10 +10,10 @@ function statement(invoice, plays) {
     for (let perf of invoice.performances) {
         volumeCredits += volumeCreditsFor(perf);
         // 청구 내역을 출력
-        result += ` ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${perf.audience}석)\n`;
+        result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
         totalAmount += amountFor(perf);
     }
-    result += `총액: ${format(totalAmount / 100)}\n`;
+    result += `총액: ${usd(totalAmount)}\n`;
     result += `적립 포인트: ${volumeCredits}점\n`;
     return result;
 }
@@ -21,7 +21,7 @@ function statement(invoice, plays) {
 function usd(aNumber) {
     return new Intl.NumberFormat("en-US", {
         style: "currency", currency: "USD", minimumFractionDigits: 2
-    }).format(aNumber);
+    }).format(aNumber / 100);
 }
 
 function volumeCreditsFor(aPerformance) {
