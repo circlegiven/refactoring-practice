66ff2af3ab8a74963ea11516c36804633af47e49
circlegiven <circlegiven@gmail.com>
2020-10-12 20:48:17 +0900

    amount 변수 인라인

--- examples/playground.js
+++ examples/playground.js
@@ -10,7 +10,6 @@ function statement(invoice, plays) {
     }).format;
 
     for (let perf of invoice.performances) {
-        const thisAmount = amountFor(perf);
 
         // 포인트를 적립
         volumeCredits += Math.max(perf.audience - 30, 0);
@@ -19,8 +18,8 @@ function statement(invoice, plays) {
             volumeCredits += Math.floor(perf.audience / 5);
         }
         // 청구 내역을 출력
-        result += ` ${playFor(perf).name}: ${format(thisAmount / 100)} (${perf.audience}석)\n`;
-        totalAmount += thisAmount;
+        result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience}석)\n`;
+        totalAmount += amountFor(perf);
     }
     result += `총액: ${format(totalAmount / 100)}\n`;
     result += `적립 포인트: ${volumeCredits}점\n`;
