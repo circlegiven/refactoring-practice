9f30c0af08d824d7fdcd7c6f9b6a86e1c53fa611
circlegiven <circlegiven@gmail.com>
2020-10-12 20:41:32 +0900

    임시 변수 play를 함수로 변경

--- examples/playground.js
+++ examples/playground.js
@@ -10,8 +10,7 @@ function statement(invoice, plays) {
     }).format;
 
     for (let perf of invoice.performances) {
-        const play = plays[perf.playID];
-
+        const play = playFor(perf);
         const thisAmount = amountFor(perf, play);
 
         // 포인트를 적립
@@ -29,22 +28,26 @@ function statement(invoice, plays) {
     return result;
 }
 
-function amountFor(perf, play) {
+function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+}
+
+function amountFor(aPerformance, play) {
     let result = 0;
 
     switch (play.type) {
         case "tragedy": // 비극
             result = 40000;
-            if (perf.audience > 30) {
-                result += 1000 * (perf.audience - 30);
+            if (aPerformance.audience > 30) {
+                result += 1000 * (aPerformance.audience - 30);
             }
             break;
         case "comedy":  // 희극
             result = 30000;
-            if (perf.audience > 20) {
-                result += 10000 + 500 * (perf.audience - 20);
+            if (aPerformance.audience > 20) {
+                result += 10000 + 500 * (aPerformance.audience - 20);
             }
-            result += 300 * perf.audience;
+            result += 300 * aPerformance.audience;
             break;
         default:
             throw new Error(`알수 없는 장르: ${play.type}`);
