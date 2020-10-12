16ea1091219d47fa87ab29bf32aab8b0061b6f64
circlegiven <circlegiven@gmail.com>
2020-10-12 20:56:44 +0900

    volumeCredits 변수를 제거하기 위해 for loop에서 분리

--- examples/playground.js
+++ examples/playground.js
@@ -6,12 +6,14 @@ function statement(invoice, plays) {
 
     let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
-    let volumeCredits = 0;
     for (let perf of invoice.performances) {
-        volumeCredits += volumeCreditsFor(perf);
         // 청구 내역을 출력
         result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
         totalAmount += amountFor(perf);
+    }
+    let volumeCredits = 0;
+    for (let perf of invoice.performances) {
+        volumeCredits += volumeCreditsFor(perf);
     }
     result += `총액: ${usd(totalAmount)}\n`;
     result += `적립 포인트: ${volumeCredits}점\n`;
