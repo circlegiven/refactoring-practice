7d1687294292808a1c7237bc728c4df80f4ed486
circlegiven <circlegiven@gmail.com>
2020-10-12 20:20:57 +0900

    amount 계산 추출

--- examples/playground.js
+++ examples/playground.js
@@ -11,25 +11,8 @@ function statement(invoice, plays) {
 
     for (let perf of invoice.performances) {
         const play = plays[perf.playID];
-        let thisAmount = 0;
 
-        switch (play.type) {
-            case "tragedy": // 비극
-                thisAmount = 40000;
-                if (perf.audience > 30) {
-                    thisAmount += 1000 * (perf.audience - 30);
-                }
-                break;
-            case "comedy":  // 희극
-                thisAmount = 30000;
-                if (perf.audience > 20) {
-                    thisAmount += 10000 + 500 * (perf.audience - 20);
-                }
-                thisAmount += 300 * perf.audience;
-                break;
-            default:
-                throw new Error(`알수 없는 장르: ${play.type}`);
-        }
+        const thisAmount = amountFor(perf, play);
 
         // 포인트를 적립
         volumeCredits += Math.max(perf.audience - 30, 0);
@@ -44,6 +27,29 @@ function statement(invoice, plays) {
     result += `총액: ${format(totalAmount / 100)}\n`;
     result += `적립 포인트: ${volumeCredits}점\n`;
     return result;
+}
+
+function amountFor(perf, play) {
+    let thisAmount = 0;
+
+    switch (play.type) {
+        case "tragedy": // 비극
+            thisAmount = 40000;
+            if (perf.audience > 30) {
+                thisAmount += 1000 * (perf.audience - 30);
+            }
+            break;
+        case "comedy":  // 희극
+            thisAmount = 30000;
+            if (perf.audience > 20) {
+                thisAmount += 10000 + 500 * (perf.audience - 20);
+            }
+            thisAmount += 300 * perf.audience;
+            break;
+        default:
+            throw new Error(`알수 없는 장르: ${play.type}`);
+    }
+    return thisAmount;
 }
 
 console.log(statement(invoices[0], plays));
