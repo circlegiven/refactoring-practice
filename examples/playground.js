f9311ff1a8c92192f0a454361a2692337889adfa
circlegiven <circlegiven@gmail.com>
2020-10-12 20:54:17 +0900

    format 함수 추출

--- examples/playground.js
+++ examples/playground.js
@@ -5,20 +5,23 @@ function statement(invoice, plays) {
     let totalAmount = 0;
 
     let result = `청구 내역 (고객명: ${invoice.customer})\n`;
-    const format = new Intl.NumberFormat("en-US", {
-        style: "currency", currency: "USD", minimumFractionDigits: 2
-    }).format;
 
     let volumeCredits = 0;
     for (let perf of invoice.performances) {
         volumeCredits += volumeCreditsFor(perf);
         // 청구 내역을 출력
-        result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${perf.audience}석)\n`;
+        result += ` ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${perf.audience}석)\n`;
         totalAmount += amountFor(perf);
     }
     result += `총액: ${format(totalAmount / 100)}\n`;
     result += `적립 포인트: ${volumeCredits}점\n`;
     return result;
+}
+
+function usd(aNumber) {
+    return new Intl.NumberFormat("en-US", {
+        style: "currency", currency: "USD", minimumFractionDigits: 2
+    }).format(aNumber);
 }
 
 function volumeCreditsFor(aPerformance) {
