fa7d1765cd201551755fbaa2f1231fbfbc76f3e4
circlegiven <circlegiven@gmail.com>
2020-10-13 20:20:08 +0900

    HTML 생성용 코드 생성

--- examples/playground.js
+++ examples/playground.js
@@ -2,11 +2,11 @@ import plays from "../data/plays.js";
 import invoices from "../data/invoices.js";
 import createStatementData from "./createStatementData.js";
 
-function htmlStatement(invoice, plays) {
-    return renderHtml(createStatementData(invoice, plays));
+function statement(invoice, plays) {
+    return renderPlainText(createStatementData(invoice, plays));
 }
 
-function renderHtml(data) {
+function renderPlainText(data, plays) {
     let result = `청구 내역 (고객명: ${data.customer})\n`;
 
     for (let perf of data.performances) {
@@ -25,4 +25,24 @@ function usd(aNumber) {
     }).format(aNumber / 100);
 }
 
-console.log(htmlStatement(invoices[0], plays));
+function htmlStatement(invoice, plays) {
+    return renderHtml(createStatementData(invoice, plays));
+}
+
+function renderHtml(data) {
+    let result = `<h1>청구 내역 (고객명: ${data.customer})</h1>\n`;
+    result += '<table>\n';
+    result += '<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>';
+    for (let perf of data.performances) {
+        // 청구 내역을 출력
+        result += ` <tr><td>${perf.play.name}</td><td>(${perf.audience}석)</td>\n`;
+        result += `<td>${usd(perf.amount)}</td></tr>\n`;
+    }
+    result += '</table>\n';
+    result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`;
+    result += `<p>적립 포인트: <em>${data.totalVolumeCredits}</em>점</p>\n`;
+    return result;
+}
+
+
+console.log(statement(invoices[0], plays));
