39bf6d2932a5ab56c90365f6a07c66118ceba362
circlegiven <circlegiven@gmail.com>
2020-10-13 19:51:10 +0900

    playFor 함수 이동

--- examples/playground.js
+++ examples/playground.js
@@ -9,7 +9,12 @@ function statement(invoice, plays) {
 
     function enrichPerformance(aPerformance) {
         const result = Object.assign({}, aPerformance);
+        result.play = playFor(result);
         return result;
+    }
+
+    function playFor(aPerformance) {
+        return plays[aPerformance.playID];
     }
 }
 
@@ -18,7 +23,7 @@ function renderPlainText(data, plays) {
 
     for (let perf of data.performances) {
         // 청구 내역을 출력
-        result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
+        result += ` ${perf.play.name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
     }
 
     result += `총액: ${usd(totalAmount())}\n`;
@@ -52,20 +57,16 @@ function renderPlainText(data, plays) {
         // 포인트를 적립
         result += Math.max(aPerformance.audience - 30, 0);
         // 희극 관객 5명 마다 추가포인트 제공
-        if ("comedy" === playFor(aPerformance).type) {
+        if ("comedy" === aPerformance.play.type) {
             result += Math.floor(aPerformance.audience / 5);
         }
         return result;
     }
 
-    function playFor(aPerformance) {
-        return plays[aPerformance.playID];
-    }
-
     function amountFor(aPerformance) {
         let result = 0;
 
-        switch (playFor(aPerformance).type) {
+        switch (aPerformance.play.type) {
             case "tragedy": // 비극
                 result = 40000;
                 if (aPerformance.audience > 30) {
@@ -80,7 +81,7 @@ function renderPlainText(data, plays) {
                 result += 300 * aPerformance.audience;
                 break;
             default:
-                throw new Error(`알수 없는 장르: ${playFor(aPerformance).type}`);
+                throw new Error(`알수 없는 장르: ${aPerformance.play.type}`);
         }
         return result;
     }
