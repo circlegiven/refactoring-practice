8e3cb3b3f01c8a9301f56a5cec9d88f07c7730cf
circlegiven <circlegiven@gmail.com>
2020-10-13 20:12:18 +0900

    함수명 변경과 USD 최상위로 이동

--- examples/playground.js
+++ examples/playground.js
@@ -2,11 +2,11 @@ import plays from "../data/plays.js";
 import invoices from "../data/invoices.js";
 import createStatementData from "./createStatementData.js";
 
-function statement(invoice, plays) {
-    return renderPlainText(createStatementData(invoice, plays));
+function htmlStatement(invoice, plays) {
+    return renderHtml(createStatementData(invoice, plays));
 }
 
-function renderPlainText(data) {
+function renderHtml(data) {
     let result = `청구 내역 (고객명: ${data.customer})\n`;
 
     for (let perf of data.performances) {
@@ -17,13 +17,12 @@ function renderPlainText(data) {
     result += `총액: ${usd(data.totalAmount)}\n`;
     result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
     return result;
-
-
-    function usd(aNumber) {
-        return new Intl.NumberFormat("en-US", {
-            style: "currency", currency: "USD", minimumFractionDigits: 2
-        }).format(aNumber / 100);
-    }
 }
 
-console.log(statement(invoices[0], plays));
+function usd(aNumber) {
+    return new Intl.NumberFormat("en-US", {
+        style: "currency", currency: "USD", minimumFractionDigits: 2
+    }).format(aNumber / 100);
+}
+
+console.log(htmlStatement(invoices[0], plays));
