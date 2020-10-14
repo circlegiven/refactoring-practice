568b696df2539e452c06fb69571f6afe12686c5b
circlegiven <circlegiven@gmail.com>
2020-10-14 20:53:43 +0900

    for 반복문 pipe 라인으로 변경

--- examples/createStatementData.js
+++ examples/createStatementData.js
@@ -72,18 +72,10 @@ export default function createStatementData(invoice, plays) {
     }
 
     function totalAmount(data) {
-        let result = 0;
-        for (let perf of data.performances) {
-            result += perf.amount;
-        }
-        return result;
+        return data.performances.reduce((total, perf) => total += perf.amount, 0);
     }
 
     function totalVolumeCredits(data) {
-        let result = 0;
-        for (let perf of data.performances) {
-            result += perf.volumeCredits;
-        }
-        return result;
+        return data.performances.reduce((total, perf) => total += perf.volumeCredits, 0);;
     }
 }
