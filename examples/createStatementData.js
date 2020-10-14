154593f80ceab1ca7088e1aba314f43807e064a5
circlegiven <circlegiven@gmail.com>
2020-10-14 20:14:16 +0900

    volumeCredits 이동

--- examples/createStatementData.js
+++ examples/createStatementData.js
@@ -26,6 +26,17 @@ class PerformanceCalculator {
         }
         return result;
     }
+
+    get volumeCredits() {
+        let result = 0;
+        // 포인트를 적립
+        result += Math.max(this.performance.audience - 30, 0);
+        // 희극 관객 5명 마다 추가포인트 제공
+        if ("comedy" === this.play.type) {
+            result += Math.floor(this.performance.audience / 5);
+        }
+        return result;
+    }
 }
 
 export default function createStatementData(invoice, plays) {
@@ -40,28 +51,13 @@ export default function createStatementData(invoice, plays) {
         const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
         const result = Object.assign({}, aPerformance);
         result.play = calculator.play;
-        result.amount = amountFor(result);
-        result.volumeCredits = volumeCreditsFor(result);
+        result.amount = calculator.amount;
+        result.volumeCredits = calculator.volumeCredits;
         return result;
     }
 
     function playFor(aPerformance) {
         return plays[aPerformance.playID];
-    }
-
-    function amountFor(aPerformance) {
-       return new PerformanceCalculator(aPerformance, playFor(aPerformance)).amount;
-    }
-
-    function volumeCreditsFor(aPerformance) {
-        let result = 0;
-        // 포인트를 적립
-        result += Math.max(aPerformance.audience - 30, 0);
-        // 희극 관객 5명 마다 추가포인트 제공
-        if ("comedy" === aPerformance.play.type) {
-            result += Math.floor(aPerformance.audience / 5);
-        }
-        return result;
     }
 
     function totalAmount(data) {
